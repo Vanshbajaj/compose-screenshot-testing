26f59f8a975dfc236f4fe20e08a2123731e32950
Vansh <vbajaj55@gmail.com>
2025-09-22 10:49:09 +0530

    Update text in MainActivity
    The text displayed in the `MainActivity` has been changed from "Hello, Compose Screenshot!" to "Hello, Compose Official Screenshot!".

--- app/src/main/java/com/example/composescreenshotofficial/MainActivity.kt
+++ app/src/main/java/com/example/composescreenshotofficial/MainActivity.kt
@@ -40,7 +40,7 @@ fun SampleScreen() {
              horizontalAlignment = Alignment.CenterHorizontally
          ) {
              Text(
-                 "Hello, Compose Screenshot!",
+                 "Hello, Compose Official Screenshot!",
                  style = MaterialTheme.typography.headlineSmall
              )
              Spacer(Modifier.height(8.dp))
