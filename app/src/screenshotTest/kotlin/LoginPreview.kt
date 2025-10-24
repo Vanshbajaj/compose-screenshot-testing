86ad957ffe7302eb0876b06a1d99dfbe46bf28d7
Vansh <vbajaj55@gmail.com>
2025-10-24 11:21:21 +0530

    refactor: Disable screenshot test for LoginScreen
    This commit comments out the `LoginScreenPreview` composable in `LoginPreview.kt`.
    
    The `@PreviewTest` annotation is disabled, effectively removing this specific preview from the screenshot testing suite.

--- app/src/screenshotTest/kotlin/LoginPreview.kt
+++ app/src/screenshotTest/kotlin/LoginPreview.kt
@@ -4,20 +4,20 @@ import androidx.navigation.compose.rememberNavController
 import com.android.tools.screenshot.PreviewTest
 import com.example.composescreenshotofficial.fakeLogin
 
-@PreviewTest
-@Preview
-@Composable
-fun LoginScreenPreview() {
-    LoginScreen(
-        onLogin = { email, password, remember ->
-            fakeLogin(email, password, remember)
-        },
-        onForgotPassword = {
-            // Handle "Forgot password" navigation
-        },
-        onSignUp = {
-            // Handle "Sign up" navigation
-        },
-        navController = rememberNavController()
-    )
-}
+//@PreviewTest
+//@Preview
+//@Composable
+//fun LoginScreenPreview() {
+//    LoginScreen(
+//        onLogin = { email, password, remember ->
+//            fakeLogin(email, password, remember)
+//        },
+//        onForgotPassword = {
+//            // Handle "Forgot password" navigation
+//        },
+//        onSignUp = {
+//            // Handle "Sign up" navigation
+//        },
+//        navController = rememberNavController()
+//    )
+//}
